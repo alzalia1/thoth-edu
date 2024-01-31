dfc1afc260073e34374e3535d530007dd95feeff
Saphi8 <saphiron888@gmail.com>
2024-01-31 19:37:14 +0100

    Changement classe n1

---
+++ Backend/CLasses/verifTexte.py
@@ -0,0 +1,43 @@
+class verifText:
+    def __init__(self):
+        """
+        Initialisation,
+        Paramètres à compléter
+        """
+        self.consigne = None
+        self.reponse = None
+        self.accent = None
+        self.pluriel = None
+        self.determinant = None
+        self.personnes = None
+        self.pronomsPersos = None
+
+    def cons(self):
+        """
+        Je sais pas comment ça doit marcher ça
+        """
+        return
+
+    def rep(self):
+        """
+        Je sais pas comment ça doit marcher ça
+        """
+        return
+
+    # Paramètres
+    # Concrètement je sais pas comment on va gérer ça alors je vais faire plusieurs façon à voir laquelle sera adoptée
+
+    def acc(self):
+        return
+
+    def plur(self):
+        return
+
+    def deter(self):
+        return
+
+    def pers(self):
+        return
+
+    def pronP(self):
+        return
