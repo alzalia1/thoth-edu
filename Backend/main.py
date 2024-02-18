66617e29efb06e9d0f1afef0c0d1af843cec0664
alzalia <alzalia436@gmail.com>
2024-02-18 21:57:13 +0100

    Mise en place définitif du système de routes d'API

--- Backend/main.py
+++ Backend/main.py
@@ -1,11 +1,25 @@
 # main.py
+
+# Pour créer une nouvelle route (accessible via https://api.thoth-edu.fr/{nom de la route}), utiliser la structure suivante :
+
+# @app.route("/{nom de la route}")
+# def {nom de la fonction associée}({arguments ?}):
+
+#     $$ contenu de la fonction
+
+#     $$ création d'un message à retourner
+
+#     return message
+
+# Il est préférable que la fonction réelle s'éxécute dans un fichier de route (voir le dossier routes)
+
 from flask import Flask, request, jsonify
 import json
 from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app, origins="https://thtoh-edu.fr")
+CORS(app, origins="https://thoth-edu.fr")
 
 # Get the directory of the current script
 script_dir = os.path.dirname(os.path.realpath(__file__))
