6d9f75971958f8e9cdc6808d06d2933d09834d03
Saphi8 <saphiron888@gmail.com>
2024-03-10 15:34:03 +0100

    Début du code sur l'inscription (changement dans main.py)

--- Backend/main.py
+++ Backend/main.py
@@ -17,9 +17,14 @@ from flask import Flask, request, jsonify
 import json
 from flask_cors import CORS
 import os
+import routes as r
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# Route pour la bdd (A MODIFIER)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://../database/user.sqlite3"
 CORS(app, origins="https://thoth-edu.fr")
+db = SQLAlchemy(app)
 
 # Get the directory of the current script
 script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -28,9 +33,18 @@ script_dir = os.path.dirname(os.path.realpath(__file__))
 data_path = os.path.join(script_dir, "data.json")
 
 
+# Création de la classe utilisateur
+class User(db.Model):
+    id = db.Column(db.String(80), unique=True, nullable=False)
+    mdp = db.Column(db.String(120), unique=True, nullable=False)
+    accents = db.Column(db.String(200), unique=False, nullable=True)
+
+
 @app.route("/")
 def home():
-    return "Hello :) Vous êtes bien arrivé sur la page 'home' du système d'API ThothEdu !"
+    return (
+        "Hello :) Vous êtes bien arrivé sur la page 'home' du système d'API ThothEdu !"
+    )
 
 
 @app.route("/save-json", methods=["POST"])
@@ -49,3 +63,13 @@ def save_json():
     response = jsonify({"message": message})
 
     return response
+
+
+@app.route("/user/signin", methods=["POST"])
+def inscription():
+    data = request.get_json()
+    # { "id" : "Bob" ; "mdp" : "mdp" ; "accents" : "é" }
+    new_user = User(id=data["id"], mdp=data["mdp"], accents=data["accents"])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User created successfully"}), 201
