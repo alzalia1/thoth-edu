0b3bfaf342154743d514ee7af44e7cdb1bbe1a67
alzalia <alzalia436@gmail.com>
2024-04-23 13:23:34 +0200

    Ajout de l'API pour accéder à une eval

--- Site/www/scripts/home.js
+++ Site/www/scripts/home.js
@@ -1,3 +1,5 @@
+// Connection
+
 const connectButton = document.getElementById("connect");
 
 connectButton.addEventListener("click", openConnectWindow);
@@ -5,3 +7,33 @@ connectButton.addEventListener("click", openConnectWindow);
 function openConnectWindow() {
     window.location.href = `https://professeur.thoth-edu.fr/`;
 }
+
+// Entering eval
+
+const validateButton = document.getElementById("validate");
+const idInputField = document.getElementById("idInputField");
+const errorConnecting = document.getElementById("errorConnecting");
+
+validateButton.addEventListener("click", () => {
+    id = idInputField.value;
+    if (!id) {
+        errorConnecting.textContent = "Veuillez entrer un id";
+    }
+
+    fetch("https://api.thoth-edu.fr/eval/check_access", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id: id }),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.exist == "yes") {
+                window.location.href = `https://thoth-edu.fr/eval?e=${id}`;
+            } else {
+                errorConnecting.textContent = "Veuillez entrer un id valide";
+            }
+        })
+        .catch((error) => console.error("Erreur lors de l'envoi des données :", error));
+});
